c908c8622fbf339e5110afa13bb887bea881b16f
Daniel Maslia <dmaslia@umich.edu>
2026-02-05 09:21:33 -0700

    still figuring this out

--- src/main/java/com/danielmaslia/masterinventory/CommandHandler.java
+++ src/main/java/com/danielmaslia/masterinventory/CommandHandler.java
@@ -219,7 +219,7 @@ public class CommandHandler implements CommandExecutor, TabCompleter {
                 for (Villager v : world.getEntitiesByClass(Villager.class)) {
                     if (v.getProfession() == Profession.NONE || v.getProfession() == Profession.NITWIT) continue;
 
-                    String profName = v.getProfession().getClass().getCanonicalName().toUpperCase();
+                    String profName = v.getProfession().toString();
                     List<MerchantRecipe> recipes = new ArrayList<>(v.getRecipes());
                     boolean modified = false;
 
